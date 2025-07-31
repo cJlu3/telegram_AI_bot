fc610e49d7e8db4fd24f23e560c052df1defa23f
yApuk_cJlu3 <mega.slizen@gmail.com>
2025-07-31 15:18:51 +0500

    rebuild by langchain_llm7

--- src/main.py
+++ src/main.py
@@ -1,23 +1,20 @@
-import openai
+from langchain_core.messages import HumanMessage
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from dotenv import dotenv_values, find_dotenv
+from langchain_llm7 import ChatLLM7
 
 logging.basicConfig(level=logging.INFO)
-
 dp = Dispatcher()
-
 settings = dotenv_values(find_dotenv(".env"))
+
 TG_BOT_TOKEN = str(settings.get("TG_BOT_TOKEN"))
 AI_MODEL = str(settings.get("AI_MODEL"))
-AI_URL = settings.get("AI_URL")
+AI_URL = str(settings.get("AI_URL"))
 AI_TOKEN = settings.get("AI_TOKEN")
 
-client = openai.OpenAI(
-    base_url=AI_URL,
-    api_key=AI_TOKEN,
-)
+llm = ChatLLM7(model=AI_MODEL, base_url=AI_URL)
 
 
 @dp.message()
@@ -26,15 +23,12 @@ async def answer_the_message(message: types.Message):
         user_prompt = str(message.text)
         logging.info(f"log: {user_prompt=}")
 
-        response = client.chat.completions.create(
-            model=AI_MODEL,
-            messages=[{"role": "user", "content": user_prompt}],
-        )
+        response = llm.invoke([HumanMessage(content=user_prompt)])
 
-        answer = str(response.choices[0].message.content)
+        answer = response.content
         logging.info(f"log: bot_answer={answer}")
 
-        await message.answer(answer)
+        await message.answer(str(answer))
     except Exception as e:
         logging.error(f"Error processing message: {e}", exc_info=True)
         await message.answer(f"Error:\n{e}")
